94948c09044301180ebad41a80c7e4dcfeaa7c69
Mathieu Leenhardt <mathieu.leenhardt@gmail.com>
2016-02-04 11:32:00 +0100

    Added grpc benchmark

--- src/csharp/Benchmark.Grpc.Server/Program.cs
+++ src/csharp/Benchmark.Grpc.Server/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Benchmark.Grpc.Common;
+using Grpc.Core;
 
 namespace Benchmark.Grpc.Server
 {
@@ -10,6 +8,18 @@ namespace Benchmark.Grpc.Server
     {
         static void Main(string[] args)
         {
+            const int port = 1337;
+            var rpcServer = new global::Grpc.Core.Server
+            {
+                Services = { BenchmarkService.BindService(new BenchmarkServiceServer()) },
+                Ports = { new ServerPort("localhost", port, ServerCredentials.Insecure) }
+            };
+            rpcServer.Start();
+
+            Console.WriteLine("BenchmarkService server listening on port " + port);
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+            rpcServer.ShutdownAsync().Wait();
         }
     }
 }
